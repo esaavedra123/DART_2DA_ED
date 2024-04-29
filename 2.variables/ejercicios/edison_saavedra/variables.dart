608be238ea296464f6c29a5db0098bc15a7f7ced
Edison Saavedra <esaavedra@digitalgo.tech>
2024-04-29 18:28:48 -0500

    update: hello word

---
+++ 2.variables/ejercicios/edison_saavedra/variables.dart
@@ -0,0 +1,18 @@
+main() {
+  var nombre = "Edison";
+  print(nombre);
+  nombre = "Edison Saavedra";
+  print(nombre);
+
+  final cantidad_carros = 8;
+  print (cantidad_carros);
+
+  const velocidad_luz =3000000;
+  print(velocidad_luz);
+
+  late String fecha;
+  fecha = '25 de noviembre del 2024';
+  print(fecha);
+
+/*  */
+}
